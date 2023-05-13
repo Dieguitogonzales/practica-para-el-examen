63583bd1efa5938a038f128aeb82600d5ed0e0b0
Dieguitogonzales <132518146+Dieguitogonzales@users.noreply.github.com>
2023-05-12 21:58:41 -0400

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,43 @@
+ /*
+ crear 10 variables que haga referencia a una persona
+  let primernombre = "diego"
+  let apellido = "gonzales"
+  /*
+  */
+ let primernombre = "diego";
+ let segundonombre = "emilio";
+ let apellido = "gonzales";
+ let edad = 32;
+ let estado_civil = "casado";
+ let celular = 78174118;
+ let carrera = "sistemas";
+ let apodo = "chavo";
+ let colegio = "vida nueva"; 
+ let hijos = 1;
+   
+ /*crear un array y asignar 6 de los valores de la persona
+ tips: let persona = ["diego"..] */
+
+ let persona = [
+    "diego","emilio","gonzales",32,"casado",78174118
+ ];
+ /* realizar la validacion si la persona es mayor de edad 
+ tips: if(<condicion>) {
+console.log(<mensaje>)
+ }; 
+ 
+ else {
+    console.log(<edad>)};*/
+
+
+ if (edad >= 18) {
+    console.log("la persona es mayor de edad")
+ }
+ else {
+    console.log("la persona es menor de edad")
+ }
+ 
+
+ 
+
+
